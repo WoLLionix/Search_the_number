c430bff91de670645d61b35df9a396ded73ba9ca
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-11-08 20:46:04 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> A(366);
+    srand(time(0));
+
+    int n;
+    std::cout << "Enter a number from 1 to 366:\n";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = rand() % 2;
+        std::cout << A[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    int firstI = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] == 1)
+        {
+            firstI = i + 1;
+            break;
+        }
+    }
+    if (firstI != -1)
+    {
+        std::cout << "The position of the first element that equal to 1:\n" << firstI << std::endl;
+    }
+    else
+        std::cout << "An element with value 1 not found\n";
+}
